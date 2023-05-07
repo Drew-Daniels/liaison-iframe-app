5812479c7cacf8e5d6ed2a8ea755b2af0616c07a
Drew Daniels <daniels_drew@protonmail.com>
2023-05-07 17:22:26 -0500

    add basic styling, modularize script

---
+++ index.js
@@ -0,0 +1,17 @@
+import { IFrame } from 'https://unpkg.com/liaison-core/lib/index.js'
+
+const iframe = IFrame({
+    // parentOrigin: 'https://drew-daniels.github.io/liaison-parent-app',
+    parentOrigin: 'http://localhost:5500',
+    effects: {
+        sayHiFromIFrame: () => {
+            console.log('Hello from iframe!')
+        }
+    }
+});
+iframe.init();
+
+const button = document.getElementById('iframe-btn');
+button.onclick = () => {
+    iframe.callParentEffect({ name: 'sayHiFromParent', args: {} })
+}
