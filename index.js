24f793828883eee4d37a791f68bff449068bd069
Drew Daniels <daniels_drew@protonmail.com>
2023-05-07 19:46:53 -0500

    add styling, functionality to send messages to parent window asynchronously

--- index.js
+++ index.js
@@ -1,17 +1,38 @@
 import { IFrame } from 'https://unpkg.com/liaison-core@latest/lib/index.js'
 
+function timeout(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
+
 const iframe = IFrame({
     // parentOrigin: 'https://drew-daniels.github.io/liaison-parent-app',
     parentOrigin: 'http://localhost:5500',
     effects: {
         sayHiFromIFrame: () => {
-            console.log('Hello from iframe!')
+            const messagesList = document.getElementById('sync-messages');
+            const message = document.createElement('li');
+            message.innerText = 'Synchronous message received from parent';
+            message.classList.add('message');
+            messagesList.appendChild(message);
+        },
+        sayHiFromIFrameAsync: async () => {
+            await timeout(3000);
+            const messagesList = document.getElementById('async-messages');
+            const message = document.createElement('li');
+            message.innerText = 'Asynchronous message received from parent';
+            message.classList.add('message');
+            messagesList.appendChild(message);
         }
     }
 });
 iframe.init();
 
-const button = document.getElementById('iframe-btn');
-button.onclick = () => {
-    iframe.callParentEffect({ name: 'sayHiFromParent', args: {} })
+const buttonSync = document.getElementById('iframe-btn-sync');
+buttonSync.onclick = () => {
+    iframe.callParentEffect({ name: 'sayHiFromParentSync', args: {} })
+}
+
+const buttonAsync = document.getElementById('iframe-btn-async');
+buttonAsync.onclick = () => {
+    iframe.callParentEffect({ name: 'sayHiFromParentAsync', args: {} })
 }
